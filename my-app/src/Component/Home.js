0bb4472fbc0349d2ebd780189a63286c76ad243e
RAHUL CHANDRAKANT CHAVAN <rahulcchavan.22@gmail.com>
2021-09-10 10:43:18 +0530

    icons added.

--- my-app/src/Component/Home.js
+++ my-app/src/Component/Home.js
@@ -65,6 +65,11 @@ class Home extends Component{
                     <div class="card-body">
                         <h5 class="card-title">Card title</h5>
                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <span style={{"font-size": "1.5em"}}>
+                        <i class="far fa-calendar-alt"></i>
+                        </span>
+                        
+                        <br/>
                         <a href="#" class="btn btn-primary">Go somewhere</a>
                     </div>
                 </div>
